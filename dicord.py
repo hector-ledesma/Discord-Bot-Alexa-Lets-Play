67dd8894f96d3846443502b976448aaa9c07d01b
Hector Ledesma <41846079+mazi89@users.noreply.github.com>
2020-04-05 15:43:00 -0400

    Change token.py file name and variable to dtoken. It conflicted with some other dependency.

--- dicord.py
+++ dicord.py
@@ -1,11 +1,9 @@
+from discordToken import dtoken
 import discord
 import os
-import token
 from discord.ext import commands
-from token import *
 
 discord = commands.Bot(command_prefix = '.')
-
 @discord.event
 async def on_ready():
     print('Alexa has loaded into the server.')
@@ -22,4 +20,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         discord.load_extension(f'cogs.{filename[:-3]}')
 
-discord.run(token)
+discord.run(dtoken)
